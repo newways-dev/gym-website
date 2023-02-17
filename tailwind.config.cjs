69f7e13085b4d956d41ca9bb98685a9d9152194c
newways-dev <reactwebdew@gmail.com>
2023-02-17 11:55:42 +0300

    config setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const { url } = require("inspector");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -14,6 +16,26 @@ module.exports = {
         "secondary-400": "#ffcd58",
         "secondary-500": "#ffc132",
       },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #ff616a 0%, #ffc837 100%)",
+        "mobile-home": "url('./src/assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./src/assets/EvolveText.png')",
+        abstractwaves: "url('./src/assets/AbstractWaves.png')",
+        sparkles: "url('./src/assets/Sparkles.png')",
+        circles: "url('./src/assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [],
